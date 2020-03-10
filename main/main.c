6b3802a1afe2ebd0e97603e8d13559e75be50b96
Dima Ses <ses.dima@gmail.com>
2020-03-10 17:37:50 +0200

    third commit

--- main/main.c
+++ main/main.c
@@ -42,6 +42,7 @@ static void event_handler(void* arg, esp_event_base_t event_base,
             ESP_LOGI(TAG, "retry to connect to the AP");
         } else {
             xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+            s_retry_num = 0;
         }
         ESP_LOGI(TAG,"connect to the AP fail");
     } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
@@ -76,108 +77,73 @@ void keyboard_input_task(void *pvParameters)//ввод текста с клавиатуры
 		printf("\n");
 		xQueueGenericSend(inputStingQueue, &pdata, (TickType_t)0, queueSEND_TO_FRONT);
 		//memset(data, 0, sizeof(data));
-		vTaskDelay(100/portTICK_PERIOD_MS);
+		vTaskDelay(pdMS_TO_TICKS(50));
 	}
 }
 
-void get_sta_conf_task(void *pvParameters)//ввод ssid и пароля
-{
-	char *prxData;	//указатель на буфер ввода с клавиатуры
-	uint8_t inputNum = 0; //порядок ввода данных
-	uint8_t printBool = 1;
-	while(true)
+void wifi_init_sta(void) {
+	static uint8_t connectionState = false;
+		s_wifi_event_group = xEventGroupCreate();
+		ESP_ERROR_CHECK(esp_event_loop_create_default());
+		wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+		ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+		ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+		ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+		ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+
+	while(connectionState != true)
 	{
-		switch(inputNum){
-		case 0:	//SSID
-			if(printBool){printf("Enter SSID: "); printBool = 0;}
-			if(inputStingQueue != 0)
-			{
-				if(xQueueGenericReceive(inputStingQueue, &prxData, (TickType_t)0, false))
-				{
-					memcpy(&wifi_config.sta.ssid, prxData, 32);//скопировать из буфера в конфигурацию
-					//printf("\nYour SSID: %s\n", wifi_config.sta.ssid);
-					memset(prxData, 0, INPUT_BUFFER_SIZE);//очистить буфер
-					inputNum++; printBool = 1;	//следующий шаг
-				}
-			}
-			break;
-		case 1:	//password
-			if(printBool){printf("Enter password: "); printBool = 0;}
-			if(inputStingQueue != 0)
-			{
-				if(xQueueGenericReceive(inputStingQueue, &prxData, (TickType_t)0, false))
-				{
-					memcpy(&wifi_config.sta.password, prxData, 32);
-					//printf("\nYour password: %s\n", wifi_config.sta.password);
-					memset(prxData, 0, INPUT_BUFFER_SIZE);
-					inputNum++; printBool = 1;
-					vTaskSuspend(keyboard_input_taskHandle);	//ввод с клавиатуры больше не нужен
-				}
-			}
-			break;
-		case 2:	//init
-			wifi_configReady = 1;	//конфигурация готова
-			vTaskSuspend(NULL);	//больше этот таск не нужен
-			break;
-		}
-		vTaskDelay(10/portTICK_PERIOD_MS);
+	char *prxData;	//указатель на буфер ввода с клавиатуры
+	vTaskResume(keyboard_input_taskHandle);
+	printf("Enter SSID: ");
+	xQueueGenericReceive(inputStingQueue, &prxData, portMAX_DELAY, false);
+	memcpy(&wifi_config.sta.ssid, prxData, 32);	//скопировать из буфера в конфигурацию
+	memset(prxData, 0, INPUT_BUFFER_SIZE);	//очистить буфер
+
+	printf("Enter password: ");
+	xQueueGenericReceive(inputStingQueue, &prxData, portMAX_DELAY, false);
+	memcpy(&wifi_config.sta.password, prxData, 32);	//скопировать из буфера в конфигурацию
+	memset(prxData, 0, INPUT_BUFFER_SIZE);	//очистить буфер
+	vTaskSuspend(keyboard_input_taskHandle);//остановить таск ввода с клавиатуры
+
+	ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+	ESP_ERROR_CHECK(esp_wifi_start());
+
+	ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+	/* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+	 * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+	EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+	WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+	pdFALSE,
+	pdFALSE,
+	portMAX_DELAY);
+
+	/* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+	 * happened. */
+	if (bits & WIFI_CONNECTED_BIT) {
+		ESP_LOGI(TAG, "connected to ap SSID:%s password:%s", wifi_config.sta.ssid, wifi_config.sta.password);
+		connectionState = true;
+	} else if (bits & WIFI_FAIL_BIT) {
+		ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s", wifi_config.sta.ssid, wifi_config.sta.password);
+		esp_wifi_stop();
+		xEventGroupClearBits(s_wifi_event_group, WIFI_FAIL_BIT);
+	} else {
+		ESP_LOGE(TAG, "UNEXPECTED EVENT");
 	}
-}
 
-void wifi_init_sta()
-{
-    s_wifi_event_group = xEventGroupCreate();
-
-    tcpip_adapter_init();
-
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
-    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
-
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
-    ESP_ERROR_CHECK(esp_wifi_start() );
-
-    ESP_LOGI(TAG, "wifi_init_sta finished.");
-
-    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
-     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
-    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
-            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
-            pdFALSE,
-            pdFALSE,
-            portMAX_DELAY);
-
-    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
-     * happened. */
-    if (bits & WIFI_CONNECTED_BIT) {
-        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
-                 wifi_config.sta.ssid, wifi_config.sta.password);
-    } else if (bits & WIFI_FAIL_BIT) {
-        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
-        		wifi_config.sta.ssid, wifi_config.sta.password);
-    } else {
-        ESP_LOGE(TAG, "UNEXPECTED EVENT");
-    }
-
-    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler));
-    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler));
-    vEventGroupDelete(s_wifi_event_group);
+	}
+	ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP,&event_handler));
+	ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler));
+	vEventGroupDelete(s_wifi_event_group);
 }
 
 void app_main(void)
 {
 	nvs_flash_init();
+	tcpip_adapter_init();
 	xTaskCreate(keyboard_input_task, "keyboard", 2048, NULL, 1, &keyboard_input_taskHandle);
-	xTaskCreate(get_sta_conf_task, "wifi init", 2048, NULL, 1, NULL);
-
-	while(wifi_configReady != 1){
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-	}
+	vTaskSuspend(keyboard_input_taskHandle);	//сразу же остановит таск ввода
 	wifi_init_sta();
 	while (true) {
 		vTaskDelay(10 / portTICK_PERIOD_MS);
