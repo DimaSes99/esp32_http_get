904e5ae6752519417de8bbd68125c29c54201527
Dima Ses <ses.dima@gmail.com>
2020-03-11 19:12:39 +0200

    commit

--- main/main.c
+++ main/main.c
@@ -5,6 +5,8 @@
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
 #include "esp_wifi.h"
+#include "esp_http_client.h"
+#include "esp_tls.h"
 #include "esp_system.h"
 #include "esp_event.h"
 #include "esp_event_loop.h"
@@ -30,7 +32,7 @@ static const char *TAG = "wifi station";
 
 static int s_retry_num = 0;
 
-static void event_handler(void* arg, esp_event_base_t event_base,
+static void wifi_event_handler(void* arg, esp_event_base_t event_base,
                                 int32_t event_id, void* event_data)
 {
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
@@ -53,6 +55,46 @@ static void event_handler(void* arg, esp_event_base_t event_base,
         xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
     }
 }
+
+esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+{
+    switch(evt->event_id) {
+        case HTTP_EVENT_ERROR:
+            ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+            break;
+        case HTTP_EVENT_ON_CONNECTED:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+            break;
+        case HTTP_EVENT_HEADER_SENT:
+            ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+            break;
+        case HTTP_EVENT_ON_HEADER:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+            break;
+        case HTTP_EVENT_ON_DATA:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+            if (!esp_http_client_is_chunked_response(evt->client)) {
+                // Write out data
+                 printf("\nReceived data:\n%.*s", evt->data_len, (char*)evt->data);
+            }
+
+            break;
+        case HTTP_EVENT_ON_FINISH:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+            break;
+        case HTTP_EVENT_DISCONNECTED:
+            ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
+            int mbedtls_err = 0;
+            esp_err_t err = esp_tls_get_and_clear_last_error(evt->data, &mbedtls_err, NULL);
+            if (err != 0) {
+                ESP_LOGI(TAG, "Last esp error code: 0x%x", err);
+                ESP_LOGI(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
+            }
+            break;
+    }
+    return ESP_OK;
+}
+
 
 void keyboard_input_task(void *pvParameters)//ввод текста с клавиатуры
 {
@@ -87,8 +129,8 @@ void wifi_init_sta(void) {
 		ESP_ERROR_CHECK(esp_event_loop_create_default());
 		wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
 		ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-		ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
-		ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+		ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL));
+		ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler, NULL));
 		ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
 
 	while(connectionState != true)
@@ -112,7 +154,7 @@ void wifi_init_sta(void) {
 	ESP_LOGI(TAG, "wifi_init_sta finished.");
 
 	/* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
-	 * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+	 * number of re-tries (WIFI_FAIL_BIT). The bits are set by wifi_event_handler() (see above) */
 	EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
 	WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
 	pdFALSE,
@@ -133,8 +175,8 @@ void wifi_init_sta(void) {
 	}
 
 	}
-	ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP,&event_handler));
-	ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler));
+	ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP,&wifi_event_handler));
+	ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler));
 	vEventGroupDelete(s_wifi_event_group);
 }
 
@@ -145,6 +187,25 @@ void app_main(void)
 	xTaskCreate(keyboard_input_task, "keyboard", 2048, NULL, 1, &keyboard_input_taskHandle);
 	vTaskSuspend(keyboard_input_taskHandle);	//сразу же остановит таск ввода
 	wifi_init_sta();
+
+	esp_http_client_config_t config = {
+	        .host = "httpbin.org",
+	        .path = "/get",
+	        .transport_type = HTTP_TRANSPORT_OVER_TCP,
+	        .event_handler = _http_event_handler,
+	    };
+	    esp_http_client_handle_t client = esp_http_client_init(&config);
+
+	    // GET
+	    esp_err_t err = esp_http_client_perform(client);
+	    if (err == ESP_OK) {
+	        ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %d",
+	                esp_http_client_get_status_code(client),
+	                esp_http_client_get_content_length(client));
+	    } else {
+	        ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
+	    }
+	    esp_http_client_cleanup(client);
 	while (true) {
 		vTaskDelay(10 / portTICK_PERIOD_MS);
 	}
